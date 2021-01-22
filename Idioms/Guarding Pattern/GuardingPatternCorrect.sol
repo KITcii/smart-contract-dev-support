c74631e6ee505cfc5951be4f042072381f5dbd1b
Niclas Kannengießer <n.kannengiesser@web.de>
2021-01-22 11:29:51 +0100

    UDATE: Renamed authorization to guarding

--- Idioms/Guarding Pattern/GuardingPatternCorrect.sol
+++ Idioms/Guarding Pattern/GuardingPatternCorrect.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.5.0 <0.7.0;
 
-contract AuthorizationCheck {
+contract FunctionGuarding {
     address public owner = msg.sender;
 
+    // Use a modifier to define your guarding conditions
     modifier onlyBy(address account){
         require(msg.sender == account, "Not authorized!");
         _;
