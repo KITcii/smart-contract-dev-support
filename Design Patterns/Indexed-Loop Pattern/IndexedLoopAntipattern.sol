4a58a71b0dc91f974892372871c1be44edd7f9c3
hasebrook <57216653+hasebrook@users.noreply.github.com>
2021-02-15 18:48:59 +0100

    Update IndexedLoopAntipattern.sol

--- Design Patterns/Indexed-Loop Pattern/IndexedLoopAntipattern.sol
+++ Design Patterns/Indexed-Loop Pattern/IndexedLoopAntipattern.sol
@@ -15,12 +15,16 @@ contract IndexedLoopAntipattern {
     
     function payout() public {
         uint256 i = 0;
+        //If call fails after i iterations due to a gas related issue, there is no way to resume the
+        //loop if calling the function again
+        
         while(i < payees.length) {
-            // The require statement will block the loop
-            // if an asset transfer always fails
-            require(payees[i].addr.{value:payees[i].value}(),
-                       "An error occured.");
+            // The require statement will block the loop if an asset transfer always fails
+            (bool success, ) = payees[i].addr.call{value:payees[i].value}("");
+            require(success, "An error occured.");
+            payees[i].value = 0;
             i++;
-        }
+        }    
+            
     }
 }
