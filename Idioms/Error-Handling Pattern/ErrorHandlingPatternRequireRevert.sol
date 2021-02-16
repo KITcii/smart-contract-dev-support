271b718510514903de5442ffd57f6e2113ece66d
hasebrook <57216653+hasebrook@users.noreply.github.com>
2021-02-16 17:05:17 +0100

    Update ErrorHandlingPatternRequireRevert.sol

--- Idioms/Error-Handling Pattern/ErrorHandlingPatternRequireRevert.sol
+++ Idioms/Error-Handling Pattern/ErrorHandlingPatternRequireRevert.sol
@@ -2,21 +2,24 @@ pragma solidity 0.6.10;
 
 contract ErrorHandlingPatternRequireRevert {
     function sendAssets(address payable addr)
-       public payable returns (bool) {
-        require (!addr.call{value: (msg.value / 2)}(""),
-                   "Asset transfer failed.");
-        return true;
+        public payable returns (bool) {
+            (bool success, ) = addr.call{value: (msg.value / 2)}("");
+            //hier stand ein Negationszeichen, falsch??
+            require (success, "Asset transfer failed.");
+            return true;
     }
 
     function sendAssetsMoreComplex(address payable addr)
-       public payable returns (bool) {
-        if(block.difficulty < 1000) {
-            if(!addr.call{value: (msg.value / 2)}("")) {
-                revert("Asset transfer failed.");
+        public payable returns (bool) {
+            if(block.difficulty < 1000) {
+                (bool success, ) = addr.call{value: (msg.value / 2)}("");
+                if(!success) {
+                    revert("Asset transfer failed.");
             } else {
                 return true;
             }
-        }      
-        return true;
-    }
+            
+            }      
+            return true;
+        }
 }
