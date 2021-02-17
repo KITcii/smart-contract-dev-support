2fc9e55ea0914292f226f314e5cf82fe984c9484
niclas2109 <n.kannengiesser@web.de>
2021-02-17 13:19:41 +0100

    Update ExternalCallPattern.sol

--- Idioms/External-Call Pattern/ExternalCallPattern.sol
+++ Idioms/External-Call Pattern/ExternalCallPattern.sol
@@ -20,10 +20,8 @@ contract ExternalCallPattern {
 
     function doSomething(address _externalAddress, string memory _text1,
       string memory _text2) public isContract(_externalAddress) {
-
         // Check if a smart contract is available at the given address to avoid, for example, asset loss when sending asset
         require(isContract(_externalAddress), "No smart contract available at given address!");
-
 
         // Instantiate ExternalContract to make direct calls with revert(…) in case of failures in the function execution
         ExternalContract c = ExternalContract(_externalAddress);
