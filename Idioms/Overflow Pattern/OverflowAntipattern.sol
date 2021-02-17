90df82007ca149121482b188c4faafab396d625b
niclas2109 <n.kannengiesser@web.de>
2021-02-17 12:30:43 +0100

    Update OverflowAntipattern.sol

--- Idioms/Overflow Pattern/OverflowAntipattern.sol
+++ Idioms/Overflow Pattern/OverflowAntipattern.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.7.0;
 
 contract OverflowAntipattern {
-    event loopCompleted(uint256 number);
-    
     uint256 counter = 0;
+    
+    event LoopCompleted(uint256 number);
     
     function runLoop() public {
        for (uint8 i=0; i < 258 ; i+1){
