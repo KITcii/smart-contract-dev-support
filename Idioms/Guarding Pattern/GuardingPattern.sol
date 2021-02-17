a14d12666268d8b97e64b19efadc25789a928a17
niclas2109 <n.kannengiesser@web.de>
2021-02-17 13:23:08 +0100

    Update GuardingPattern.sol

--- Idioms/Guarding Pattern/GuardingPattern.sol
+++ Idioms/Guarding Pattern/GuardingPattern.sol
@@ -4,13 +4,13 @@ contract GuardingPattern {
     address public owner = msg.sender;
 
     // Use a modifier to define your guarding conditions
-    modifier onlyBy(address account){
-        require(msg.sender == account, "Not authorized!");
+    modifier onlyBy(address _account){
+        require(owner == _account, "Not authorized!");
         _;
     }
 
-    function changeOwner(address newOwner) public
-        onlyBy(owner){
-        owner = newOwner;
+    function changeOwner(address _newOwner) public
+        onlyBy(msg.sender){
+        owner = _newOwner;
     }
 }
