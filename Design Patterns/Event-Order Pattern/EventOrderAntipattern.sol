d5742b81e54c45b3cba6719b71ec9c1b95ff9654
Michelle Pfister <michelle.pfister@gmx.de>
2021-02-17 11:42:50 +0100

    Updated External Call Pattern

--- Design Patterns/Event-Order Pattern/EventOrderAntipattern.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.7.0;
- 
-contract EventOrderAntipattern {
-
-    function a() {
-        // Code to be executed in any state
-    }
-}
--- Idioms/External-Call Pattern/ExternalCallAntipattern_1.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.7.0;
-
-contract ExternalContract {
-    function externalFunction(string memory text1, string memory text2)
-       public pure returns (bool) {
-        return keccak256(bytes(text1)) == keccak256(bytes(text2));
-    }
-}
