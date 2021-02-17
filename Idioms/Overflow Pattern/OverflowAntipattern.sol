a03d26f7f426c2825f4889f2a44ec22b7f47084c
hasebrook <57216653+hasebrook@users.noreply.github.com>
2021-02-17 12:03:18 +0100

    Update OverflowAntipattern.sol

--- Idioms/Overflow Pattern/OverflowAntipattern.sol
+++ Idioms/Overflow Pattern/OverflowAntipattern.sol
@@ -6,7 +6,7 @@ contract OverflowAntipattern {
     uint256 counter = 0;
     
     function runLoop() public {
-        for (uint8 i=255; i < 258 ; i+1){
+        for (uint8 i=0; i < 258 ; i+1){
             // Your code
             counter += 1; 
             
