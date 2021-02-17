17200253ffdf799d26fe8afbea7db995ef30ef0d
niclas2109 <n.kannengiesser@web.de>
2021-02-17 13:01:57 +0100

    Update EventOrderPattern.sol

--- Design Patterns/Event-Order Pattern/EventOrderPattern.sol
+++ Design Patterns/Event-Order Pattern/EventOrderPattern.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.7.0;
 contract EventOrderPattern {
     uint256 transCounter = 0;
 
-    modifier transitionCounting(uint256 nextTransNum){
-         require(nextTransNum == transCounter,
+    modifier transitionCounting(uint256 _nextTransNum){
+         require(transCounter == _nextTransNum,
                 "Current smart contract state does not match targeted state.");
          transCounter += 1;
          _;
@@ -12,7 +12,8 @@ contract EventOrderPattern {
 }
 
 contract EventOrder is TransitionCounter {
-    function a(uint nextTransNum) public transitionCounting(nextTransNum) {
+
+    function a(uint _nextTransNum) public transitionCounting(_nextTransNum) {
         // Code to be executed in a certain state
     }
 }
