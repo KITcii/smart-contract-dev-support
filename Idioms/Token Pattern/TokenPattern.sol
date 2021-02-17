17195ab0fbcbca4cd6408eca8857bb54f4724196
hasebrook <57216653+hasebrook@users.noreply.github.com>
2021-02-17 09:13:00 +0100

    Update TokenPattern.sol

--- Idioms/Token Pattern/TokenPattern.sol
+++ Idioms/Token Pattern/TokenPattern.sol
@@ -1,10 +1,8 @@
 pragma solidity >=0.5.0 <0.7.0;
 
-// Separate the logic from the actual token storage in favor of maintenance.
-
 contract TokenPattern {
     address public minter;
-    mapping (address => uint) public userBalances;
+    mapping (address => uint) public balances;
     event Sent(address from, address to, uint amount);
 
     constructor() public {
@@ -12,17 +10,26 @@ contract TokenPattern {
     }
 
     function mint(address receiver, uint amount) public returns(uint256) {
-        // Equals function mint(…) of the wrong example lines 17-20
+        require(msg.sender == minter);
+        
+        balances[receiver] += amount;
+        return balances[receiver];
     }
 
     function send(address receiver, uint amount) public returns(bool) {
-        // Equals function send(…) of the wrong example lines 24-29
+        require(amount <= balances[msg.sender], "Insufficient balance.");
+        
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+        return true;
     }
+
 }
 
 contract LogicContract {
     address owner;
-    TokenContract t;
+    TokenPattern t;
     address public highest_bidder;
     uint256 highest_bid;
 
@@ -32,13 +39,14 @@ contract LogicContract {
     
     function changeToken (address _address) public {
         require(msg.sender == owner);
-        t = TokenContract(_address);
+        t = TokenPattern(_address);
     }
 
     function auction(uint256 bid) public {
-        require(t.userBalances(msg.sender) >= bid, "Insufficient balance.");
+        require(t.balances(msg.sender) >= bid, "Insufficient balance.");
         require(bid>highest_bid, "Your bid is too low.");
         
         highest_bidder=msg.sender;
         highest_bid=bid;
     }
+}    
