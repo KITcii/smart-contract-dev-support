182af69dfec966724b349ea79b0e808b2ee8fc54
niclas2109 <n.kannengiesser@web.de>
2021-02-17 13:20:30 +0100

    Update ExternalCallAntipattern_2.sol

--- Idioms/External-Call Pattern/ExternalCallAntipattern_2.sol
+++ Idioms/External-Call Pattern/ExternalCallAntipattern_2.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.7.0;
 contract CallerContract {
     event Response(bool success, bytes data);
 
-    function doSomething(address externalAddress, string memory text) public {
-        (bool success, bytes memory data) = externalAddress.call(
-            abi.encodeWithSignature("externalFunction(string)", text)
+    function doSomething(address _externalAddress, string memory _text) public {
+        (bool success, bytes memory data) = _externalAddress.call(
+            abi.encodeWithSignature("externalFunction(string)", _text)
         );
 
         // Check if function has been executed successfully
