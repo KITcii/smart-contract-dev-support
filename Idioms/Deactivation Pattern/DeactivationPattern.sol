ad86a64f13cf10ef217f529ed92194dcd6ac5d16
neni45902 <48184762+neni45902@users.noreply.github.com>
2021-03-11 15:41:49 +0100

    Create DeactivationPattern.sol

---
+++ Idioms/Deactivation Pattern/DeactivationPattern.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.7.0;
+
+contract ChecksEffectsInteractionsPattern {
+    bool _activated = true; 
+    
+    modifier checkActive(){
+      require (_activated);
+    }
+    
+    function anyFunction() checkActive public {
+      //code to be reverted by deactivation 
+    }
+    
+}
