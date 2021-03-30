d58feb8e2aa75f597be54ed5437dfd720e18a499
neni45902 <48184762+neni45902@users.noreply.github.com>
2021-03-30 10:18:50 +0200

    Update ReplayProtectionAntipattern.sol

--- Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
+++ Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
@@ -1,25 +1,8 @@
 pragma solidity 0.7.0;
 
 contract ReplayProtectionAntipattern {
-  function transferProxy(address _from, address _to, uint256 _value, uint256 _fee,
-    uint8 _v, bytes32 _r, bytes32 _s) public returns (bool){
-
-    if(balances[_from] < _fee + _value || _fee > _fee + _value) revert();
-
-    uint256 nonce = nonces[_from];
-    bytes32 h = keccak256(_from,_to,_value,_fee,nonce);
-    if(_from != ecrecover(h,_v,_r,_s)) revert();
-
-    if(balances[_to] + _value < balances[_to]
-        || balances[msg.sender] + _fee < balances[msg.sender]) revert();
-    balances[_to] += _value;
-    emit Transfer(_from, _to, _value);
-
-    balances[msg.sender] += _fee;
-    emit Transfer(_from, msg.sender, _fee);
-
-    balances[_from] -= _value + _fee;
-    nonces[_from] = nonce + 1;
-    return true;
-  }
+    
+    function execute(uint256 _operationType, address _to, uint256 _value, bytes calldata _data, uint256 _executionNonce, bytes calldata _signature) external {
+        IdentityContractLib.execute(owner, _executionNonce, _operationType, _to, _value, _data, _signature);
+    }
 }
