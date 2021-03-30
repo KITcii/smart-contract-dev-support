24275b6b27b53756e50b77bd75b17c56991f231b
niclas2109 <n.kannengiesser@web.de>
2021-03-30 14:45:41 +0200

    Update ReplayProtectionPattern.sol

--- Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.sol
+++ Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.sol
@@ -3,20 +3,32 @@ pragma solidity 0.7.0;
 contract ReplayProtectionPattern{
     uint256 executionNonce = 0;
     
+    mapping<address, uint256> balances;
+    
     function execute(uint256 _operationType, address _to, uint256 _value,
         bytes calldata _data, uint256 _executionNonce, bytes calldata _signature)
         external {
-        // Limit the number of execution nonces that can be skipped to avoid overflows.
+        // Limit the number of execution nonces that can be skipped
         require(_executionNonce >= executionNonce && _executionNonce <= executionNonce + 1e9);
+        
+        // Check signature
+        // TODO
         
         // Increment the stored nonce
         executionNonce = _executionNonce + 1;
         
         // Execute the function
-        someFunction();
+        transferTokens();
     }
     
-    function someFunction() private {
-        // Do something
+    function transferTokens() private {
+        require(balances[msg.sender] > 0, "No funds available!");
+        uint256 value = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(value);
+    }
+    
+    function receiveFunds() external payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
     }
 }
