403cc7e5d1e6e4ed87350f2840c6e6c1641c7858
niclas2109 <n.kannengiesser@web.de>
2021-03-30 14:52:41 +0200

    Update ReplayProtectionPattern.sol

--- Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.sol
+++ Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.sol
@@ -1,15 +1,14 @@
 pragma solidity 0.7.0;
 
 contract ReplayProtectionPattern{
-    uint256 executionNonce = 0;
+    uint256 private executionNonce = 0;
+    mapping(address => uint256) private balances;
     
-    mapping<address, uint256> balances;
-    
-    function execute(uint256 _operationType, address _to, uint256 _value,
-        bytes calldata _data, uint256 _executionNonce, bytes calldata _signature)
-        external {
+    function execute(address _to, uint256 _value, uint256 _executionNonce,
+        bytes calldata _signature) external {
         // Limit the number of execution nonces that can be skipped
-        require(_executionNonce >= executionNonce && _executionNonce <= executionNonce + 1e9);
+        require(_executionNonce >= executionNonce && _executionNonce <= executionNonce + 1e9,
+            "Invalid execution nonce!");
         
         // Check signature
         // TODO
@@ -18,14 +17,20 @@ contract ReplayProtectionPattern{
         executionNonce = _executionNonce + 1;
         
         // Execute the function
-        transferTokens();
+        transferTokens(msg.sender, _to, _value);
     }
     
-    function transferTokens() private {
+    function transferTokens(address _from, address _to, uint256 _amount) private {
+        require(balances[_from] > _amount, "Not enough funds available!");
+        balances[_from] = balances[_from] - _amount;
+        balances[_to] = balances[_to] + _amount;
+    }
+    
+    function withdrawTokens() private {
         require(balances[msg.sender] > 0, "No funds available!");
-        uint256 value = balances[msg.sender];
+        uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        msg.sender.transfer(value);
+        msg.sender.transfer(amount);
     }
     
     function receiveFunds() external payable {
