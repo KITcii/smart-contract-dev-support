b7abf13b2e03663ef1a73b43d8cf10cef0cc0006
niclas2109 <n.kannengiesser@web.de>
2021-03-30 15:18:44 +0200

    Update ReplayProtectionAntipattern.sol

--- Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
+++ Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
@@ -1,8 +1,42 @@
 pragma solidity 0.7.0;
 
-contract ReplayProtectionAntipattern {
+contract ReplayProtectionPattern{
+    uint256 private executionNonce = 0;
+    mapping(address => uint256) private balances;
     
-    function execute(uint256 _operationType, address _to, uint256 _value, bytes calldata _data, uint256 _executionNonce, bytes calldata _signature) external {
-        IdentityContractLib.execute(owner, _executionNonce, _operationType, _to, _value, _data, _signature);
+    address owner;
+    
+    modifier replayProtection(address _from, address _to,
+        uint256 _amount, uint256 _executionNonce) {
+        // Limit the number of execution nonces that can be skipped
+        require(_executionNonce >= executionNonce && _executionNonce <= executionNonce + 1e9,
+            "Invalid execution nonce!");
+       
+        // Increment the stored nonce
+        executionNonce = _executionNonce + 1;
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function transferTokens(address _from, address _to, uint256 _amount, uint256 _executionNonce, bytes calldata _signature)
+        external replayProtection(_executionNonce, _signature) {
+        require(balances[_from] > _amount, "Not enough funds available!");
+        require(msg.sender == owner, "You are not the owner!");
+        balances[_from] = balances[_from] - _amount;
+        balances[_to] = balances[_to] + _amount;
+    }
+
+    function buyTokens() external payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+    }
+    
+    function withdrawTokens() external {
+        require(balances[msg.sender] > 0, "No funds available!");
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(amount);
     }
 }
