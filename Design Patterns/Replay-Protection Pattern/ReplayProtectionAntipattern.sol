2c1bdda623e45db9c4f75a18c58bb5130fd44b86
niclas2109 <n.kannengiesser@web.de>
2021-03-30 15:21:39 +0200

    Update ReplayProtectionAntipattern.sol

--- Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
+++ Design Patterns/Replay-Protection Pattern/ReplayProtectionAntipattern.sol
@@ -7,11 +7,11 @@ contract ReplayProtectionPattern{
     address owner;
     
     modifier replayProtection(address _from, address _to,
-        uint256 _amount, uint256 _executionNonce) {
+        uint256 _amount, uint256 _executionNonce, bytes calldata _signature) {
         // Limit the number of execution nonces that can be skipped
         require(_executionNonce >= executionNonce && _executionNonce <= executionNonce + 1e9,
             "Invalid execution nonce!");
-       
+
         // Increment the stored nonce
         executionNonce = _executionNonce + 1;
         _;
@@ -22,7 +22,7 @@ contract ReplayProtectionPattern{
     }
 
     function transferTokens(address _from, address _to, uint256 _amount, uint256 _executionNonce, bytes calldata _signature)
-        external replayProtection(_executionNonce, _signature) {
+        external replayProtection(_from, _to, _amount, _executionNonce, _signature) {
         require(balances[_from] > _amount, "Not enough funds available!");
         require(msg.sender == owner, "You are not the owner!");
         balances[_from] = balances[_from] - _amount;
