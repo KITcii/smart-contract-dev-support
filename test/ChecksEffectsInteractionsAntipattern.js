f03a7e736cb3cba880ea1476f30cf278003fcf66
Niklas <advancesinfinancialmachine@gmail.com>
2021-04-25 19:10:54 +0200

    Adding Test

--- test/ChecksEffectsInteractionsAntipattern.js
+++ test/ChecksEffectsInteractionsAntipattern.js
@@ -1,9 +1,13 @@
 const ChecksEffectsInteractionsAntipattern = artifacts.require("ChecksEffectsInteractionsAntipattern");
 contract("ChecksEffectsInteractionsAntipattern", (accounts) => {
     let [alice, bob] = accounts;
-    it("should be able to do something", async () => {
+    let contractInstance;
+    beforeEach(async () => {
         const contractInstance = await ChecksEffectsInteractionsAntipattern.new();
+    });
 
-        
+    it("should be able to do something", async () => {
+       const result = await contractInstance.receive({from: alice});
+       assert.equal(result.receipt.status, true);
     })
 })
