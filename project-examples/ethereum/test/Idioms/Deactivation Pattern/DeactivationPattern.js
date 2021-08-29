171236221359f339322b50b7975e1b76142c76e9
Niklas <advancesinfinancialmachine@gmail.com>
2021-08-29 13:30:17 +0200

    Added modifiers and constructors

--- project-examples/ethereum/test/Idioms/Deactivation Pattern/DeactivationPattern.js
+++ project-examples/ethereum/test/Idioms/Deactivation Pattern/DeactivationPattern.js
@@ -8,21 +8,8 @@ contract('DeactivationPattern', async (accounts) => {
         contract = await DeactivationPattern.new();
     })
 
-    it('DeactivationPattern balance should starts with 0 ETH', async () => {
-        let balance = await web3.eth.getBalance(contract.address);
-        assert.equal(balance, 0);
-    })
-
-    it('DeactivationPattern balance should have 1 ETH after deposit', async () => {
-        let one_eth = web3.utils.toWei("1", "ether");
-        await web3.eth.sendTransaction({from: accounts[1], to: contract.address, value: one_eth});
-        let balance_wei = await web3.eth.getBalance(contract.address);
-        let balance_ether = web3.utils.fromWei(balance_wei, "ether");
-        assert.equal(balance_ether, 1);
-    })
-
     it('Should produce an error if deactivated and funds are send to it.', async () => {
-        await contract.deactivate();
+        await contract.setActive(false);
         let one_eth = web3.utils.toWei("1", "ether");
 
         await expectRevert(
