324d403850c3549c08e25024f027f40212af0f18
Niklas H <uiesx@student.kit.edu>
2021-08-30 15:20:14 +0200

    Added Commitment Pattern Tests (not working)

--- project-examples/ethereum/test/Design Patterns/Commitment Pattern/CommitmentPattern.js
+++ project-examples/ethereum/test/Design Patterns/Commitment Pattern/CommitmentPattern.js
@@ -1,40 +1,35 @@
 const CommitmentPattern = artifacts.require('CommitmentPattern');
 const {expectRevert} = require('@openzeppelin/test-helpers');
 
+
 contract('CommitmentPattern', async (accounts) => {
 
-    beforeEach(async () => {
+    before(async () => {
         contract = await CommitmentPattern.new({from: accounts[0]});
     })
 
     //development idea preliminary
 
     it('Should allow to make a commit ', async () => { 
-        await contract.changeOwner(accounts[1], {from: accounts[0]});
-        let owner = await contract.owner.call();
-        assert.equal(accounts[1], owner);
+        secret_commit = web3.utils.keccak256("Hello World!");
+        secret_salt = web3.utils.keccak256("42");
+        console.log(secret_salt);
+        tx = await contract.commit(secret_commit, secret_salt);
+        console.log(tx);
+        
     })
 
     it('Should not allow to reveal incorrect value', async () => {
-        await expectRevert(
-            contract.changeOwner(accounts[1], {from: accounts[1]}),
-            "Not authorized!"
-        );    
+        tx = await contract.reveal("Hello", "42");
     })
 
     it('Should only allow to reveal as commiter', async () => {
-        await expectRevert(
-            contract.changeOwner(accounts[1], {from: accounts[1]}),
-            "Not authorized!"
-        );    
+        await contract.reveal("Hello World!","42", {from: accounts[1]});
     })
 
 
     it('Should allow to reveal correct value', async () => {
-        await expectRevert(
-            contract.changeOwner(accounts[1], {from: accounts[1]}),
-            "Not authorized!"
-        );    
+        await contract.reveal("Hello World!","42");
     })
 
 
