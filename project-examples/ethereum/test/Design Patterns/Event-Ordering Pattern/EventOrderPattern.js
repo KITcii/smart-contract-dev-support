326fc61bbcb4dfee30daa8ef360eae2bed9801d2
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:10:49 +0200

    Update EventOrderPattern.js

--- project-examples/ethereum/test/Design Patterns/Event-Ordering Pattern/EventOrderPattern.js
+++ project-examples/ethereum/test/Design Patterns/Event-Ordering Pattern/EventOrderPattern.js
@@ -2,7 +2,6 @@ const EventOrder = artifacts.require('EventOrder');
 const {expectRevert} = require('@openzeppelin/test-helpers');
 
 contract('EventOrder', async (accounts) => {
-
     before(async () => {
         contract = await EventOrder.new();
     })
@@ -15,9 +14,6 @@ contract('EventOrder', async (accounts) => {
         await expectRevert(
             contract.a(1),
             "Current smart contract state does not match targeted state."
-        ); 
-        
+        );
     })
-
-
 })    
