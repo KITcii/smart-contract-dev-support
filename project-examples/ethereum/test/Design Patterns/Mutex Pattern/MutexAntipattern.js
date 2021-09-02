ba9f0b2775c7021c54c2795f83bfcb4d1471e6ba
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:19:03 +0200

    Update MutexAntipattern.js

--- project-examples/ethereum/test/Design Patterns/Mutex Pattern/MutexAntipattern.js
+++ project-examples/ethereum/test/Design Patterns/Mutex Pattern/MutexAntipattern.js
@@ -6,36 +6,36 @@ contract('MutexAntipattern', async (accounts) => {
   
     before(async () => {
         victim = await MutexAntipattern.new();
-    })
+    });
 
     it('MutexAntipattern balance should starts with 0 ETH', async () => {
-        let balance = await web3.eth.getBalance(victim.address);
+        const balance = await web3.eth.getBalance(victim.address);
         assert.equal(balance, 0);
-    })
+    });
 
     it('MutexAntipattern balance should have 11 ETH after deposit', async () => {
-        let eleven_eth = web3.utils.toWei("11", "ether");
-        await web3.eth.sendTransaction({from: accounts[1], to: victim.address, value: eleven_eth});
-        let balance_wei = await web3.eth.getBalance(victim.address);
-        let balance_ether = web3.utils.fromWei(balance_wei, "ether");
-        assert.equal(balance_ether, 11);
-    })
+        const elevenEth = web3.utils.toWei("11", "ether");
+        await web3.eth.sendTransaction({from: accounts[1], to: victim.address, value: elevenEth});
+        const balanceWei = await web3.eth.getBalance(victim.address);
+        const balanceEther = web3.utils.fromWei(balanceWei, "ether");
+        assert.equal(balanceEther, 11);
+    });
 
     it('MutexAntipattern should be safe from Reentrancy', async () => {
         // Create new attacker contract
         attacker = await attacker_contract.new(victim.address);
         
         // Send 1 ETH to the attacker contract
-        const one_eth = web3.utils.toWei("1", "ether");
+        const oneEth = web3.utils.toWei("1", "ether");
         // await web3.eth.sendTransaction({from: accounts[1], to: attacker.address, value: one_eth});
        
         // The attacker needs to have some balance at the exploitet contract
-        await attacker.setBalance({value: one_eth});
+        await attacker.setBalance({value: oneEth});
         
         // We can now try to withdraw more than we initially send to the exploited contract
-        result = await attacker.attack();
+        await attacker.attack();
         
         // Assert that the exploit has not worked
-        assert.equal(one_eth, await web3.eth.getBalance(attacker.address));
-    })
+        assert.equal(oneEth, await web3.eth.getBalance(attacker.address));
+    });
 })
