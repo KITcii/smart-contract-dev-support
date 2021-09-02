18e9f7cae765338b1ef0dbaaa70bc8361f66a5e8
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:10:16 +0200

    Update CommitmentPattern.js

--- project-examples/ethereum/test/Design Patterns/Commitment Pattern/CommitmentPattern.js
+++ project-examples/ethereum/test/Design Patterns/Commitment Pattern/CommitmentPattern.js
@@ -1,22 +1,18 @@
 const CommitmentPattern = artifacts.require('CommitmentPattern');
 const {expectRevert} = require('@openzeppelin/test-helpers');
 
-
 contract('CommitmentPattern', async (accounts) => {
-
     before(async () => {
         contract = await CommitmentPattern.new({from: accounts[0]});
     })
 
-    //development idea preliminary
+    // Development idea preliminary
 
     it('Should allow to make a commit ', async () => { 
         secret_commit = web3.utils.keccak256("Hello World!");
         secret_salt = web3.utils.keccak256("42");
         console.log(secret_salt);
         tx = await contract.commit(secret_commit, secret_salt);
-        //console.log(tx);
-        
     })
 
     it('Should not allow to reveal incorrect value', async () => {
@@ -27,10 +23,7 @@ contract('CommitmentPattern', async (accounts) => {
         await contract.reveal("Hello World!","42", {from: accounts[1]});
     })
 
-
     it('Should allow to reveal correct value', async () => {
         await contract.reveal("Hello World!","42");
     })
-
-
 })
