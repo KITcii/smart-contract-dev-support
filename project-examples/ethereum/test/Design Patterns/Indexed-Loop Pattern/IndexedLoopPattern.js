b253f93e2666ce0f7412c053c7f92ffb8843571a
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:11:58 +0200

    Update IndexedLoopPattern.js

--- project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
+++ project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
@@ -8,7 +8,7 @@ contract('IndexedLoopPattern', async (accounts) => {
     })
 
     it('Should be possible to send money ', async () => { 
-        console.log(await web3.eth.getBalance(accounts[0]))
+        console.log(await web3.eth.getBalance(accounts[0]));
         let amount = web3.utils.toWei("0.00001", "ether");
         for(i = 0; i<=500; i++) {
             await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
