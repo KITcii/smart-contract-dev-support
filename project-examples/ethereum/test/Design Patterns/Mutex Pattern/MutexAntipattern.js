b2bcf02d9d3e39e1bb7b6d5c1f634bf415d0f29f
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:12:52 +0200

    Update MutexAntipattern.js

--- project-examples/ethereum/test/Design Patterns/Mutex Pattern/MutexAntipattern.js
+++ project-examples/ethereum/test/Design Patterns/Mutex Pattern/MutexAntipattern.js
@@ -22,20 +22,20 @@ contract('MutexAntipattern', async (accounts) => {
     })
 
     it('MutexAntipattern should be safe from Reentrancy', async () => {
-        // create new attacker contract
+        // Create new attacker contract
         attacker = await attacker_contract.new(victim.address);
         
-        // send 1 ETH to the attacker contract
+        // Send 1 ETH to the attacker contract
         const one_eth = web3.utils.toWei("1", "ether");
         // await web3.eth.sendTransaction({from: accounts[1], to: attacker.address, value: one_eth});
        
-        // the attacker needs to have some balance at the exploitet contract
+        // The attacker needs to have some balance at the exploitet contract
         await attacker.setBalance({value: one_eth});
         
-        // we can now try to withdraw more than we initially send to the exploited contract
+        // We can now try to withdraw more than we initially send to the exploited contract
         result = await attacker.attack();
         
-        // assert that the exploit has not worked
+        // Assert that the exploit has not worked
         assert.equal(one_eth, await web3.eth.getBalance(attacker.address));
     })
 })
