e874c484a8018554906694b548d842129a482b31
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:20:05 +0200

    Update IndexedLoopPattern.js

--- project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
+++ project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
@@ -5,7 +5,7 @@ const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 contract('IndexedLoopPattern', async (accounts) => {
     before(async () => {
         contract = await IndexedLoopPattern.new();
-    })
+    });
 
     it('Should be possible to send money ', async () => { 
         console.log(await web3.eth.getBalance(accounts[0]));
@@ -14,7 +14,7 @@ contract('IndexedLoopPattern', async (accounts) => {
             await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
         }
         console.log(await web3.eth.getBalance(accounts[0]));
-    })
+    });
 
     it('Payout possible', async () => { 
         console.log(await web3.eth.getBalance(accounts[0]));
@@ -23,5 +23,5 @@ contract('IndexedLoopPattern', async (accounts) => {
         await contract.payout();
         console.log(await web3.eth.getBalance(accounts[0]));
        
-    })
-})    
+    });
+})
