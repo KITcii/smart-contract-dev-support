a059c73534181837f541549e28d1f3825077c988
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:19:28 +0200

    Update IndexedLoopAntipattern.js

--- project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopAntipattern.js
+++ project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopAntipattern.js
@@ -4,7 +4,7 @@ const {expectRevert} = require('@openzeppelin/test-helpers');
 contract('IndexedLoopAntipattern', async (accounts) => {
     before(async () => {
         contract = await IndexedLoopAntipattern.new();
-    })
+    });
 
     it('Should be possible to send money ', async () => { 
         let amount = web3.utils.toWei("0.00001", "ether");
@@ -12,10 +12,9 @@ contract('IndexedLoopAntipattern', async (accounts) => {
         for(i = 0; i<=500; i++) {
             await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
         }
-    })
+    });
 
     it('Payout possible', async () => { 
        await contract.payout();
-        
-    })
+    });
 })    
