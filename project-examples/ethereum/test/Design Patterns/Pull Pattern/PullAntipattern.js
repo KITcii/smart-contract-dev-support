e11c08f013fa87beecbc512953cd4cdf31018cf7
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:13:25 +0200

    Update PullAntipattern.js

--- project-examples/ethereum/test/Design Patterns/Pull Pattern/PullAntipattern.js
+++ project-examples/ethereum/test/Design Patterns/Pull Pattern/PullAntipattern.js
@@ -2,7 +2,6 @@ const PullAntipattern = artifacts.require('PullAntipattern');
 const {expectRevert} = require('@openzeppelin/test-helpers');
 
 contract('PullAntipattern', async (accounts) => {
-
     before(async () => {
         contract = await PullAntipattern.new();
     })
@@ -10,18 +9,15 @@ contract('PullAntipattern', async (accounts) => {
     it('Should be possible to send money ', async () => { 
         let amount = web3.utils.toWei("0.00001", "ether");
 
-        // we need accounts and not transactions
+        // We need accounts and not transactions
         for(i = 0; i<=2; i++) {
-            //new_account = await web3.personal.newAccount()
-            //await web3.eth.sendTransaction({from: new_account, to: contract.address, value: amount});
+            // new_account = await web3.personal.newAccount()
+            // await web3.eth.sendTransaction({from: new_account, to: contract.address, value: amount});
             await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
         }
     })
 
     it('Payout possible', async () => { 
-       await contract.payout();
-        
+       await contract.payout(); 
     })
-
-
 })    
