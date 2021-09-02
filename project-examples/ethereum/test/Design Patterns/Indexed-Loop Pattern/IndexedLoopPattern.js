0fd5c00b0dbff478061804349bc1e6b20b9c4968
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:11:42 +0200

    Update IndexedLoopPattern.js

--- project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
+++ project-examples/ethereum/test/Design Patterns/Indexed-Loop Pattern/IndexedLoopPattern.js
@@ -3,7 +3,6 @@ const {expectRevert} = require('@openzeppelin/test-helpers');
 const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 
 contract('IndexedLoopPattern', async (accounts) => {
-
     before(async () => {
         contract = await IndexedLoopPattern.new();
     })
@@ -14,18 +13,15 @@ contract('IndexedLoopPattern', async (accounts) => {
         for(i = 0; i<=500; i++) {
             await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
         }
-        console.log(await web3.eth.getBalance(accounts[0]))
-       
+        console.log(await web3.eth.getBalance(accounts[0]));
     })
 
     it('Payout possible', async () => { 
-        console.log(await web3.eth.getBalance(accounts[0]))
+        console.log(await web3.eth.getBalance(accounts[0]));
         await contract.payout();
-        console.log(await web3.eth.getBalance(accounts[0]))
+        console.log(await web3.eth.getBalance(accounts[0]));
         await contract.payout();
-        console.log(await web3.eth.getBalance(accounts[0]))
+        console.log(await web3.eth.getBalance(accounts[0]));
        
     })
-
-
 })    
