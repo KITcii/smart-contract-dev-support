bdd47b27c11fc54e91d95441f722576b2313ff4e
niclas2109 <n.kannengiesser@web.de>
2021-09-02 09:24:40 +0200

    Update GuardingAntipattern.js

--- project-examples/ethereum/test/Idioms/Guarding Pattern/GuardingAntipattern.js
+++ project-examples/ethereum/test/Idioms/Guarding Pattern/GuardingAntipattern.js
@@ -5,20 +5,18 @@ contract('GuardingAntipattern', async (accounts) => {
 
     beforeEach(async () => {
         contract = await GuardingAntipattern.new({from: accounts[0]});
-    })
+    });
 
     it('Should allow the owner to change the owner ', async () => { 
         await contract.changeOwner(accounts[1], {from: accounts[0]});
         let owner = await contract.owner.call();
         assert.equal(accounts[1], owner);
-    })
+    });
 
     it('Should not allow someone else to change the owner ', async () => {
         await expectRevert(
             contract.changeOwner(accounts[1], {from: accounts[1]}),
             "Not authorized!"
         );    
-    })
-
-
+    });
 })
