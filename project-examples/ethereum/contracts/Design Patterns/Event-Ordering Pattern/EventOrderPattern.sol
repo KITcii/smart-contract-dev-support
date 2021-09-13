f07e40c19c27b4d24b4bcca54545215e70a5d824
Dominik <dominik.pre@gmail.com>
2021-09-13 18:24:36 +0200

    Fix formatting, variable type consistency and state visibility

--- project-examples/ethereum/contracts/Design Patterns/Event-Ordering Pattern/EventOrderPattern.sol
+++ project-examples/ethereum/contracts/Design Patterns/Event-Ordering Pattern/EventOrderPattern.sol
@@ -3,20 +3,23 @@
 pragma solidity ^0.7.0;
 
 contract EventOrderPattern {
-    uint256 transCounter = 0;
+    uint256 internal transCounter = 0;
 
-    modifier transitionCounting(uint256 _currentTransNumber){
-         require(transCounter == _currentTransNumber,
-                "Current smart contract state does not match current transaction number.");
-         transCounter += 1;
-         _;
+    modifier transitionCounting(uint256 _currentTransNumber) {
+        require(
+            transCounter == _currentTransNumber,
+            "Current smart contract state does not match current transaction number."
+        );
+        transCounter += 1;
+        _;
     }
 }
+
 
 contract EventOrder is EventOrderPattern {
     event TransactionOccured(uint256 transNum);
 
-    function a(uint _currentTransNumber) public transitionCounting(_currentTransNumber) {
+    function a(uint256 _currentTransNumber) public transitionCounting(_currentTransNumber) {
         // Code to be executed in a certain state
         emit TransactionOccured(transCounter);
     }
