f9b3088b0c6dd6ddb2c4198733dece7865fb2321
Niklas H <uiesx@student.kit.edu>
2021-09-15 13:53:57 +0200

    Updated truffle config

--- project-examples/ethereum/truffle-config.js
+++ project-examples/ethereum/truffle-config.js
@@ -34,7 +34,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_directory: "./",
+  contracts_directory: "./contracts",
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
