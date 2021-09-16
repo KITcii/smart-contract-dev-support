d5cdb366f5411d635e495824341a66e7e0299662
Dominik <dominik.pre@gmail.com>
2021-09-17 01:07:25 +0200

    Fix styleguide

--- project-examples/ethereum/contracts/Design Patterns/Commitment Pattern/CommitmentPattern.sol
+++ project-examples/ethereum/contracts/Design Patterns/Commitment Pattern/CommitmentPattern.sol
@@ -16,7 +16,10 @@ contract CommitmentPattern {
     event Reveal(bytes32 value, bytes32 salt);
 
     function commit(bytes32 _secretValue, bytes32 _secretSalt) public {
-        require(userCommits[msg.sender].secretValue == "", "Your commitment cannot be changed anymore.");
+        require(
+            userCommits[msg.sender].secretValue == "",
+            "Your commitment cannot be changed anymore."
+        );
 
         UserCommit memory uC = userCommits[msg.sender];
         uC.secretValue = _secretValue;
@@ -29,12 +32,22 @@ contract CommitmentPattern {
 
     function reveal(string memory _plainValue, string memory _plainSalt) public {
         require(userCommits[msg.sender].secretValue != "", "You did not commit to a value.");
-        require(userCommits[msg.sender].secretSalt == keccak256(abi.encode(_plainSalt)), "Your salt does not match the original one.");
-        require(userCommits[msg.sender].secretValue == keccak256(abi.encodePacked(_plainValue, _plainSalt)), "Invalid values.");
+        require(
+            userCommits[msg.sender].secretSalt == keccak256(abi.encode(_plainSalt)),
+            "Your salt does not match the original one."
+        );
+        require(
+            userCommits[msg.sender].secretValue ==
+                keccak256(abi.encodePacked(_plainValue, _plainSalt)),
+            "Invalid values."
+        );
 
         userCommits[msg.sender].plainValue = _plainValue;
         userCommits[msg.sender].plainSalt = _plainSalt;
 
-        emit Reveal(keccak256(abi.encodePacked(_plainValue, _plainSalt)), keccak256(abi.encode(_plainSalt)));
+        emit Reveal(
+            keccak256(abi.encodePacked(_plainValue, _plainSalt)),
+            keccak256(abi.encode(_plainSalt))
+        );
     }
 }
