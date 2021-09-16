3ebabdb06715a7435d7e9b8f92b9ce9f69ec767e
Dominik <dominik.pre@gmail.com>
2021-09-16 23:36:28 +0200

    Fix styleguide and variable type consistency

--- project-examples/ethereum/contracts/Design Patterns/Mutex Pattern/MutexAntipattern.sol
+++ project-examples/ethereum/contracts/Design Patterns/Mutex Pattern/MutexAntipattern.sol
@@ -9,12 +9,11 @@ contract MutexAntipattern {
         balances[msg.sender] += msg.value;
     }
 
-    function withdraw(uint _amount) external {
+    function withdraw(uint256 _amount) external {
         require(balances[msg.sender] >= _amount, "No balance to withdraw.");
         // This is wrong according to the Checks-Effects-Interaction Pattern.
         // For demonstration purposes only!
         msg.sender.call{value: _amount}("");
-        balances[msg.sender] -= _amount;
-        
+        balances[msg.sender] -= _amount;        
     }
 }
--- project-examples/ethereum/contracts/Design Patterns/Mutex Pattern/MutexPattern.sol
+++ project-examples/ethereum/contracts/Design Patterns/Mutex Pattern/MutexPattern.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.7.0;
 
 contract MutexPattern {
-    bool locked = false;
+    bool internal locked = false;
     mapping(address => uint256) public balances;
 
     modifier noReentrancy {
@@ -17,7 +17,7 @@ contract MutexPattern {
         balances[tx.origin] += msg.value;
     }
 
-    function withdraw(uint _amount) public noReentrancy {
+    function withdraw(uint256 _amount) public noReentrancy {
         require(balances[tx.origin] >= _amount, "No balance to withdraw.");
 
         // This is wrong according to the Checks-Effects-Interaction Pattern.
