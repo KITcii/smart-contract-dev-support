d44ac5812921c0585d0a68f1a8bf0fbfe5319dd1
Niclas Kannengießer <n.kannengiesser@web.de>
2021-09-16 11:33:53 +0200

    UPDATE: Added PullPattern Test

--- project-examples/ethereum/contracts/Design Patterns/Pull Pattern/PullPattern.sol
+++ project-examples/ethereum/contracts/Design Patterns/Pull Pattern/PullPattern.sol
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity 0.7.0;
 
 contract PullPattern {
-    mapping (address => uint256) public balances;
+    mapping (address => uint) public balances;
+    event AccountBalance(uint balance);
     
     receive() external payable {
         balances[msg.sender] = msg.value;
     }
 
+    function getBalance() external {
+        emit AccountBalance(balances[msg.sender]);
+    }
+
     function payout() public {
         require(balances[msg.sender] > 0, "No balance available.");
 
-        uint256 amount = balances[msg.sender];
+        uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        msg.sender.call{value: amount}("");
+
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Transaction failed.");
+
+        emit AccountBalance(balances[msg.sender]);
     }
 }
--- project-examples/ethereum/test/Design Patterns/Pull Pattern/PullPattern.js
+++ project-examples/ethereum/test/Design Patterns/Pull Pattern/PullPattern.js
@@ -1,23 +1,24 @@
 const PullPattern = artifacts.require('PullPattern');
-const {expectRevert} = require('@openzeppelin/test-helpers');
+const {expectEvent} = require('@openzeppelin/test-helpers');
 
 contract('PullPattern', async (accounts) => {
     before(async () => {
         contract = await PullPattern.new();
     });
 
-    it('Should be possible to send money ', async () => { 
-        let amount = web3.utils.toWei("0.00001", "ether");
+    it('Should be possible to send money ', async () => {
+        const amount = web3.utils.toWei("0.00001", "ether");
 
-        // We need accounts and not transactions
-        for(i = 1; i<550; i++) {
+        for (let i = 0; i < 500; i++) {
             await web3.eth.sendTransaction({from: accounts[i], to: contract.address, value: amount});
-        };
+        }
 
-        await web3.eth.sendTransaction({from: accounts[0], to: contract.address, value: amount});
+        const balance = await contract.getBalance({from: accounts[499]});
+        await expectEvent(balance, 'AccountBalance', {balance: amount + ''});
     });
 
-    it('Payout possible', async () => { 
-       await contract.payout();
+    it('Payout possible', async () => {
+        const balance = await contract.payout({from: accounts[499]});
+        await expectEvent(balance, 'AccountBalance', {balance: '0'});
     });
-})    
+});
