9c5f5cf280c160623605048e617505a262001e79
Niklas H <uiesx@student.kit.edu>
2021-11-21 19:35:48 +0100

    Rename

--- project-examples/ethereum/test/Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.js
+++ project-examples/ethereum/test/Design Patterns/Replay-Protection Pattern/ReplayProtectionPattern.js
@@ -15,7 +15,8 @@ contract('ReplayProtectionPattern', async (accounts) => {
         const transferAmount = web3.utils.toWei('0.00001', 'ether');
         const executionNonce = 0;
 
-        const params = accounts[0].address + "" + accounts[1].address +"" + transferAmount + "" + executionNonce;
+        // const params = accounts[0].address + "" + accounts[1].address +"" + transferAmount + "" + executionNonce;
+        const params = accounts[0] + "" + accounts[1] +"" + transferAmount + "" + executionNonce;
         const signedParams = await web3.eth.sign(params, accounts[0]);
 
         console.log(params);
